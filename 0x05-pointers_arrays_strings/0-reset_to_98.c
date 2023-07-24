1058f8d7c852e9f250596b4e3f6d02bad342aeab
SenatorAgbo <agbovictor585@gmail.com>
2023-07-24 05:45:57 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * rest_to_98 - takes a pointer to an int and updates
+ * the value it points to 98
+ *
+ * @n: An integer input printer
+ * 
+ * Return: nothing
+ */
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
