b3f88d3d215cff7965bf2fbe981b95d99a8787c1
SenatorAgbo <agbovictor585@gmail.com>
2023-08-30 17:12:59 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,1 +1,36 @@
+#include "lists.h"
 
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: head of a list
+ *
+ * Return: the address of the node where the loop starts
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *p2;
+	listint_t *prev;
+
+	p2 = head;
+	prev = head;
+	if (!head)
+		return (NULL);
+
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+
+	return (NULL);
+}
