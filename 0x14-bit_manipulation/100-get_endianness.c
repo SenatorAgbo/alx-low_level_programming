6162e5510f16e6d4362f83d1b5cfe5a8f91579f2
SenatorAgbo <agbovictor585@gmail.com>
2023-09-01 09:05:11 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *c = (char *) &i;
+
+	if (*c == 1)
+		return (1);
+
+	return (*c);
+}
