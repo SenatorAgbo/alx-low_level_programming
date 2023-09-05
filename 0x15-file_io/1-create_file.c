304380c78749b225e2145aaf49d149b387234b7c
SenatorAgbo <agbovictor585@gmail.com>
2023-09-04 18:55:30 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,33 @@
 #include "main.h"
-#include <stdlib.h>
-/**
- * read_textfile - function that reads a text file and prints it to STDOUT
- * @filename: readable text
- * @letter: number of the letters to be read
- *
- * Return: w- the actual number of letters it could read and print
- * if filename is NULL return 0
- * if write fails or does not write the expected amount of bytes, return 0
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-        ssize_t fd;
-        char *buf;
-        ssize_t fd;
-        ssize_t q;
-        
-        fd = fopen (filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
-	fclose(fd);
-	return (w);
+/**
+ * create_file - Creates a file.
+ * @filename: A ptr to the name of the file to create.
+ * @text_content: A ptr to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
